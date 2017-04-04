76994ea1c864e8805276df238c74d84d1f708c45
James Fera <james.fera@gmail.com>
2017-04-04 13:18:57 -0400

    nuget

---
+++ pack.bat
@@ -0,0 +1,3 @@
+dotnet pack src\WebSocketManager -o ..\.. --include-source
+dotnet pack src\WebSocketManager.Common -o ..\.. --include-source
+dotnet pack src\WebSocketManager.Client -o ..\.. --include-source
---
+++ push.bat
@@ -0,0 +1,1 @@
+nuget push WebSocketManager.1.0.1.nupkg -Source https://www.myget.org/F/canerectors/api/v2/package && nuget push WebSocketManager.Client.1.0.0.nupkg -Source https://www.myget.org/F/canerectors/api/v2/package && nuget push WebSocketManager.Common.1.0.0.nupkg -Source https://www.myget.org/F/canerectors/api/v2/package && del *.nupkg
